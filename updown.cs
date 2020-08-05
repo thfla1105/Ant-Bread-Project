405157d12580da300296fb9a4f673133850457d0
Yonjae <50789964+Yonjae@users.noreply.github.com>
2020-08-05 11:07:25 +0900

    Add files via upload

---
+++ updown.cs
@@ -0,0 +1,48 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class updown : MonoBehaviour
+{
+    public float speed;
+
+    void Update()
+    {
+        //누르는 동안 hold
+        if ( 
+               (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.S))
+            || (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.D))
+            || (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.J))
+            || (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.K))
+            || (Input.GetKeyDown(KeyCode.A) && Input.GetKeyDown(KeyCode.L))
+
+            || (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.D))
+            || (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.J))
+            || (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.K))
+            || (Input.GetKeyDown(KeyCode.S) && Input.GetKeyDown(KeyCode.L))
+
+            || (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.J))
+            || (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.K))
+            || (Input.GetKeyDown(KeyCode.D) && Input.GetKeyDown(KeyCode.L))
+
+            || (Input.GetKeyDown(KeyCode.J) && Input.GetKeyDown(KeyCode.K))
+            || (Input.GetKeyDown(KeyCode.J) && Input.GetKeyDown(KeyCode.L))
+
+            || (Input.GetKeyDown(KeyCode.K) && Input.GetKeyDown(KeyCode.L))
+            )
+        {
+            //   transform.position += Vector3.up;
+            transform.position += new Vector3(0, 0.5f, 0);
+        }
+
+        
+        else
+            //    transform.position += Vector3.down;
+            transform.position -= new Vector3(0, 0.01f, 0);
+
+    
+    }
+}
---
+++ 오뒤_l.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class 오뒤_l : MonoBehaviour
+{
+
+    public int speed;
+
+    void Start()
+    {
+        speed = 200;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.L))   //누르는 동안 hold
+        {
+            // 단순히 위로 이동하는 것 ... 잘 작동됨 !
+            // transform.position += Vector3.up;
+
+            // 회전하는 것.... 잘 작동됨!
+            //   transform.Rotate(Vector3.up * speed * Time.deltaTime);
+            transform.localScale += new Vector3(50, 25, 25);
+
+        }
+        if (Input.GetKeyUp(KeyCode.L))  //손 떼면 복구
+        {
+            // 회전하는 것.... 잘 작동됨!
+            // transform.Rotate(Vector3.down * speed * Time.deltaTime);
+            transform.localScale -= new Vector3(50, 25, 25);
+
+
+        }
+    }
+
+}
