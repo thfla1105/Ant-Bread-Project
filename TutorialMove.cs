0c51231e7befeb42c1e3f2bbde4b5ca423680036
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-07 23:22:38 +0900

    Add files via upload

---
+++ TutorialMove.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialMove : MonoBehaviour
+{
+    GameObject ant;
+    Transform tr;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        ant = GameObject.Find("ant");
+        tr = ant.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float height = tr.position.y;
+        if(height >= -1.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
