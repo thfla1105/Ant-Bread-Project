dc55bba0edb1ba40e1d641b5db93e7902c1d3d1c
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-07 23:21:27 +0900

    Add files via upload

---
+++ AntBar.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntBar : MonoBehaviour
+{
+    RectTransform rt;
+    Transform tr;
+
+    public float upPoint;
+    public float downPoint;
+    public float barLength;
+
+    float Ant2Dx, Ant2Dy, Ant2Dht, curAntPosition, treeLength, curLength, ratio, plusLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        treeLength = upPoint - downPoint;
+
+        rt = (RectTransform)gameObject.transform;
+        tr = GameObject.Find("ant").GetComponent<Transform>();
+
+        Ant2Dx = rt.localPosition.x;
+        Ant2Dy = rt.localPosition.y;
+        Ant2Dht = rt.rect.height;
+
+        barLength -= Ant2Dht;
+
+        ratio = barLength / treeLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        curAntPosition = tr.position.y;
+        curLength = curAntPosition - downPoint;
+
+        plusLength = curLength * ratio;
+
+        rt.anchoredPosition = new Vector2(Ant2Dx, Ant2Dy + plusLength);
+    }
+}
