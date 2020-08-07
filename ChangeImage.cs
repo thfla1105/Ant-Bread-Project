9dc339c7cc2bca036aca82bf04f989815890b4ff
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-07 23:21:56 +0900

    Add files via upload

---
+++ ChangeImage.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeImage: MonoBehaviour
+{
+    public void StartButtonEnterImage()
+    {
+        RectTransform rt = (RectTransform)gameObject.transform;
+        rt.sizeDelta = new Vector2(350,148);
+        rt.anchoredPosition = new Vector2(-70, -21);
+    }
+
+    public void StartButtonExitImage()
+    {
+        RectTransform rt = (RectTransform)gameObject.transform;
+        rt.sizeDelta = new Vector2(350, 100);
+        rt.anchoredPosition = new Vector2(-70, -45);
+    }
+}
