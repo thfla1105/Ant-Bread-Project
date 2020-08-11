1946f3882239530558ba3a1581d32db4b3308cf4
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-11 18:24:22 +0900

    Add files via upload

---
+++ BreadBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreadBar : MonoBehaviour
+{
+    AntMove Antmove;
+    GameObject Ant;
+    GameObject King;
+    Transform tr;
+    public float maxPoint = 38.0f;
+
+    void Start()
+    {
+        Ant = GameObject.Find("ant");
+        Antmove = Ant.GetComponent<AntMove>();
+        King = GameObject.Find("KingImage");
+        tr = Ant.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        if (tr.eulerAngles.y == 270.0f)
+        {
+            King.SetActive(true);
+            gameObject.SetActive(false);
+        }   
+    }
+}
