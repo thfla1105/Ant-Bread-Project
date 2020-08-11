fd67425567739f033532a3d62647c40cf379c643
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-11 18:24:46 +0900

    Add files via upload

---
+++ KingBar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KingBar : MonoBehaviour
+{
+    GameObject Ant;
+    GameObject Bread;
+    Transform tr;
+    public float minPoint = 1.0f;
+    void Start()
+    {
+        Ant = GameObject.Find("ant");
+        Bread = GameObject.Find("BreadImage");
+        tr = Ant.GetComponent<Transform>();
+    }
+
+    void Update()
+    {
+        //Debug.Log(tr.rotation.x);
+        if (tr.eulerAngles.y == 90.0f)
+        {
+            Bread.SetActive(true);
+            gameObject.SetActive(false);
+        }
+    }
+}
