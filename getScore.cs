7bb0481338d9a686d951c212996dee02bf70645b
Yonjae <50789964+Yonjae@users.noreply.github.com>
2020-08-11 19:30:27 +0900

    Add files via upload
    점수 타이머 글씨체

---
+++ getScore.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class getScore : MonoBehaviour
+{
+    //식빵 잡으면 true로 바뀌도록
+    //왼앞 왼가 왼뒤 오앞 오가 오뒤
+    public bool[] breadBool = new[] { false, false, false, false, false, false };
+
+    //breadBool 에서 true 개수 count할 변수
+    int breadCount = 0;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))   //누르는 동안 hold
+        {
+
+            for(int i = 0; i < 6; i++)
+            {
+                if (breadBool[i] == true)
+                    breadCount++;
+            }
+            transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+            score.scoreValue+=breadCount;
+
+
+
+        }
+
+    }
+}
---
+++ score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class score : MonoBehaviour
+{
+    public static int scoreValue = 0;
+    Text Score;
+
+    void Start()
+    {
+        Score = GetComponent<Text>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Score.text = "Score  " + scoreValue;
+    }
+}
