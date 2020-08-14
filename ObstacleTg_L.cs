34265aa7cbbc100aae2e9128be0abef5dce7587b
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-14 19:45:27 +0900

    Add files via upload

---
+++ ObstacleTg_L.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleTg_L : MonoBehaviour
+{
+    AntMove antmove;
+
+    void Awake()
+    {
+        antmove = GameObject.Find("ant").GetComponent<AntMove>();
+    }
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag == "Bark" || col.tag == "Worm" || col.tag == "Hole" || col.tag == "AntFriend")
+        {
+            antmove.btn[5, 0] = 0;
+        }
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Bark" || col.tag == "Worm" || col.tag == "Hole" || col.tag == "AntFriend")
+        {
+            antmove.btn[5, 0] = 0;
+        }
+    }
+}
