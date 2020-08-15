23e6f1aa1c6c24c018f04f9c9a21e480edac9ca0
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-15 15:45:01 +0900

    Update ObstacleTg_S.cs

--- ObstacleTg_S.cs
+++ ObstacleTg_S.cs
@@ -24,4 +24,14 @@ public class ObstacleTg_S : MonoBehaviour
             antmove.btn[1, 0] = 0;
         }
     }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag == "Bark" || col.tag == "Worm" || col.tag == "Hole" || col.tag == "AntFriend")
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                antmove.btn[1, 0] = 1;
+            }
+        }
+    }
 }
