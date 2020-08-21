9c5e4e6c802ee010cd9df1f339dc45effc18226c
Yonjae <50789964+Yonjae@users.noreply.github.com>
2020-08-21 20:42:03 +0900

    타이머수정
    시간 종료되면 씬 넘어가는 거 추가했습니다

---
+++ timer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class timer : MonoBehaviour
+{
+
+    public GameObject textDisplay;
+    public int secondsLeft = 59;
+    public bool takingAway = false;
+
+
+    void Start()
+    {
+        textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+
+
+    }
+
+    void Update()
+    {
+        if (takingAway == false && secondsLeft > 0)
+        {
+            StartCoroutine(TimerTake());
+        }
+
+        if(secondsLeft == 0)
+        {
+            SceneManager.LoadScene("End");
+        }
+
+    }
+
+    IEnumerator TimerTake()
+    {
+        takingAway = true;
+        yield return new WaitForSeconds(1);
+        secondsLeft -= 1;
+        textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+        takingAway = false;
+    }
+
+
+
+}
