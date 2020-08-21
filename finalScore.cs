82f87b0b0f1005f9724d7138389ac13aeca68fbc
Yonjae <50789964+Yonjae@users.noreply.github.com>
2020-08-21 20:15:41 +0900

    End 씬
    end 씬 스크립트

---
+++ finalScore.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class finalScore : MonoBehaviour
+{
+    public Text finalScore1;
+    public Text highScore;
+
+    void Start()
+    {
+        finalScore1 = GetComponent<Text>();
+        highScore.text = "최고기록 " + PlayerPrefs.GetInt("highScore",0).ToString() + "개";
+
+    }
+
+    void Update()
+    {
+        finalScore1.text = score.scoreValue.ToString() + "개를 가져왔구나";
+
+        if(score.scoreValue > PlayerPrefs.GetInt("highScore", 0))
+        {
+            PlayerPrefs.SetInt("highScore", score.scoreValue);
+            highScore.text = score.scoreValue.ToString();
+        }
+
+
+
+    }
+}
