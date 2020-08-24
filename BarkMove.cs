f87901e115472038986c61552c072364f3a938c9
bsa0322 <68186101+bsa0322@users.noreply.github.com>
2020-08-24 21:34:12 +0900

    Add files via upload

---
+++ BarkMove.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarkMove : MonoBehaviour
+{
+    AntMove Ant;
+    public float time = 60.0f;
+
+    void Start()
+    {
+        Ant = GameObject.Find("ant").GetComponent<AntMove>();
+        this.gameObject.transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+        //StartCoroutine(DestroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        if (Ant.chk == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Bark" || col.tag == "Worm" || col.tag == "Hole")
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
+
+}
---
+++ HoleMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoleMove : MonoBehaviour
+{
+    AntMove Ant;
+    public float time = 60.0f;
+    void Start()
+    {
+        Ant = GameObject.Find("ant").GetComponent<AntMove>();
+        this.gameObject.transform.eulerAngles = new Vector3(0.0f, 0.0f, 0.0f);
+        //StartCoroutine(DestroySelf());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Ant.chk == 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+    void OnTriggerStay(Collider col)
+    {
+        if (col.tag == "Hole" || col.tag == "AntFriend")
+        {
+            Destroy(gameObject);
+        }
+    }
+    IEnumerator DestroySelf()
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(this.gameObject);
+    }
+}
