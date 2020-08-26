0ee6c40b5d2e9fa4bfe052dd647408346fa36d56
Yonjae <50789964+Yonjae@users.noreply.github.com>
2020-08-26 22:16:02 +0900

    falling sound

--- SoundManager.cs
+++ SoundManager.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 public class SoundManager : MonoBehaviour
 {
 
-    public static AudioClip fallingsound, pointsound, pickupsound, obstaclesound;
+    public static AudioClip fallingsound, pointsound, pickupsound, obstaclesound, falling, descendingsound;
     static AudioSource audioSrc;
+
+    static AudioSource audioSrc2;
 
     void Start()
     {
@@ -14,14 +16,24 @@ public class SoundManager : MonoBehaviour
         pointsound = Resources.Load<AudioClip>("pointsound");
         pickupsound = Resources.Load<AudioClip>("pickupsound");
         obstaclesound = Resources.Load<AudioClip>("obstaclesound");
-
+        falling = Resources.Load<AudioClip>("falling");
+        descendingsound = Resources.Load<AudioClip>("descendingsound");
 
         audioSrc = GetComponent<AudioSource>();
+        audioSrc2 = GetComponent<AudioSource>();
+
+        audioSrc2.clip = falling;
+        audioSrc2.volume = 0.02f;
+        //audioSrc2.time = 2;
+        //audioSrc2.loop = true;
+
 
     }
 
     void Update()
     {
+
+
     }
 
     public static void PlaySound(string clip)
@@ -41,6 +53,13 @@ public class SoundManager : MonoBehaviour
                 break;
             case "obstaclesound":
                 audioSrc.PlayOneShot(obstaclesound);
+                break;
+            case "falling":
+                audioSrc.PlayOneShot(falling);
+                break;
+            case "descendingsound":
+             //   audioSrc.PlayDelayed(2);
+                audioSrc.PlayOneShot(descendingsound);
                 break;
 
         }
